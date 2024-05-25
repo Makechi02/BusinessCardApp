21155fffd0c6d0c14d0becbc59f183747f869926
Makechi <makechieric9@gmail.com>
2024-05-25 11:02:51 +0300

    Minor UI changes

--- app/src/main/java/com/makechi/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/makechi/businesscardapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.makechi.businesscardapp
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -29,6 +28,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -77,10 +77,14 @@ fun ComposeHeading(modifier: Modifier = Modifier) {
             Text(
                 text = stringResource(id = R.string.name),
                 fontWeight = FontWeight.Bold,
-                fontSize = 48.sp,
+                fontSize = 32.sp,
+                textAlign = TextAlign.Center,
                 modifier = Modifier.padding(bottom = 8.dp)
             )
-            Text(text = stringResource(id = R.string.title))
+            Text(
+                text = stringResource(id = R.string.title),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
@@ -95,19 +99,25 @@ fun ComposeDetails(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Column {
-            Row(modifier = Modifier.padding(bottom = 8.dp)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(bottom = 8.dp)
+            ) {
                 Icon(Icons.Outlined.Call, contentDescription = null)
                 Spacer(modifier = Modifier.width(width = 20.dp))
                 Text(text = stringResource(R.string.phone_number), fontWeight = FontWeight.Bold)
             }
 
-            Row(modifier = Modifier.padding(bottom = 8.dp)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(bottom = 8.dp)
+            ) {
                 Icon(Icons.Outlined.Home, contentDescription = null)
                 Spacer(modifier = Modifier.width(width = 20.dp))
                 Text(text = stringResource(R.string.home), fontWeight = FontWeight.Bold)
             }
 
-            Row {
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 Icon(Icons.Outlined.Email, contentDescription = null)
                 Spacer(modifier = Modifier.width(width = 20.dp))
                 Text(text = stringResource(R.string.email), fontWeight = FontWeight.Bold)
@@ -116,7 +126,7 @@ fun ComposeDetails(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardAppTheme {
